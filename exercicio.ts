c2440d97f04fca1f5812f2fb90909eb2253a6d1b
Antony Rafael <133931539+antonyrf@users.noreply.github.com>
2025-01-13 22:02:31 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,8 @@
+function multiplicaNumero(num1: number, num2: number):number {
+    return num1 * num2;
+}
+
+function nomeMaisTexto(nome: string): string {
+    const saudacao = "Olá " + nome; 
+    return saudacao;
+}
